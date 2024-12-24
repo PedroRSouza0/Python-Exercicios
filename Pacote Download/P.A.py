5a1995bebf555a84b1443783c447379ad15f0fda
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2024-12-24 10:17:12 -0300

    P.A python com tratamento de erro

---
+++ Pacote Download/P.A.py
@@ -0,0 +1,35 @@
+from time import sleep
+i = 0 
+opcao = ' '
+print('P.A(zada) GAME')
+
+primeiro = int(input('Digite o primeiro termo da P.A: '))
+termos = int(input('Digite o número de termos desta P.A: '))
+razao = int(input('Qual será a razão da P.A?: '))
+
+while i < termos:
+    print(F'{primeiro}', end = ' -> ')
+    primeiro += razao
+    i += 1
+print('FIM')    
+
+while opcao not in ('N', 'n'):
+    opcao = str(input("Gostaria de ver mais alguns termos? [s/n]: ")).strip()
+    
+    if opcao not in ('N', 'n', 's', 'S'):
+        print('Porfavor digite uma opcão válida')
+    elif opcao not in ('N', 'n'):
+        novo = int(input('Quantos termos?: '))
+        termos += novo
+
+        for i in range (i, termos):
+            print(f'{primeiro}', end= ' -> ')
+            primeiro += razao
+            i += 1
+
+        print('PAUSE')
+        
+print("FIM")
+print("Saindo...")
+sleep(1)
+print(f'Programa executado com {termos} progressões.')
