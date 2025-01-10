62fc78d561c7d0c505200c7083bc4356b6fd810a
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2025-01-10 13:48:40 -0300

    Update 074Gg Analise de Dados.py

--- Pacote Download/Tuplas/074Gg Analise de Dados.py
+++ Pacote Download/Tuplas/074Gg Analise de Dados.py
@@ -1,12 +1,25 @@
-import random
-i = 0
-tupla = tuple(random.randint(1, 100)for i in range(5))
+from random import randint
 
-print(f'Os numero sorteados foram: {tupla}')
+# Gera 5 números aleatórios e armazena em uma tupla
+numero = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
 
-for i in tupla:
-    if tupla[i] > tupla[i+1]:
-        print(f'O maior número foi {tupla[i]}')
-      
+# Exibe os números sorteados
+print(f"Os números sorteados foram: {', '.join(map(str, numero))}")
 
-    
+# Encontra o maior número
+#maior = numero[0]
+#for n in numero:
+ #   if n > maior:
+  #      maior = n
+
+
+# Exibe o maior número
+# print("O maior número sorteado foi {}".format(maior))
+
+print(f"O maior valor sorteado foi {max(numero)}")
+
+menor = numero[0]
+for n in numero:
+    if n < menor:
+        menor = n
+print(f"O menor {menor}")        
