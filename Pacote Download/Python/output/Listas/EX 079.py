5f78e316a2d87335c842a937b1e2b35d01694170
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2025-01-28 19:09:03 -0300

    ex 079 i am backing to program

---
+++ Pacote Download/Python/output/Listas/EX 079.py
@@ -0,0 +1,23 @@
+listanum = []
+
+while True:
+    num = int(input('Digite um número: '))
+
+    if num in listanum:
+        print('O Número digitado já existe, porfavor digite outro.')
+    else:
+        listanum.append(num)
+        print('Número cadastrado com sucesso')
+
+        while True:
+            opcao = str(input('Deseja continuar? [S/N]: ')).upper().strip()
+
+            if opcao == 'S':
+                break
+            elif opcao == 'N':
+                print('\nFinal\n')
+                print(F'A Lista final foi: {sorted(listanum)}')
+                exit() # Encerra o programa
+            else:    
+                print('Porfavor digite uma opção válida',end= ' ')
+  
