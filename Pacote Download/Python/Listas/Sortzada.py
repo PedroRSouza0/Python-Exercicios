9b7c87f65fbb50c93259aafb76e0bdc7cabc7de2
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2025-02-01 14:34:14 -0300

    studying

--- Pacote Download/Python/Listas/Sortzada.py
+++ Pacote Download/Python/Listas/Sortzada.py
@@ -1,12 +1,18 @@
 from random import randint
 sortlist = []
-
 for i in range(0,5):
     sortlist.append(randint(1, 50))
     
-for ii in range(len(sortlist)): #Trava uma posição
-    for j in range(len(sortlist)): #Vai comparar com as outras
-        if sortlist[ii] < sortlist[j]:
-            sortlist[j], sortlist[ii] = sortlist[ii], sortlist[j]
-            
+
+    #bubble sort
+for ii in range(len(sortlist)): #Controla o número de repetições necessarias
+    for j in range(len(sortlist) - 1): #Compara com a mesma variavel uma casa a frente
+        if sortlist[j] < sortlist[j + 1]:
+            sortlist[j + 1], sortlist[j] = sortlist[j], sortlist[j + 1]   
 print(sortlist)
+
+    #selection sort
+'''for ii in range(len(sortlist)): #Trava uma posição
+    for j in range(len(sortlist) : #Vai comparar com a variavel travada
+        if sortlist[ii] < sortlist[j]:
+            sortlist[j], sortlist[ii] = sortlist[ii], sortlist[j]'''
