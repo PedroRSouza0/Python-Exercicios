ed26fe5a013f36f21b8d3b67e84d0cf40845072f
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2025-02-03 15:46:48 -0300

    corrected

--- Pacote Download/Python/Listas/EX 083.py
+++ Pacote Download/Python/Listas/EX 083.py
@@ -1,17 +1,20 @@
 expression = list()
 keys = closekeys = 0
 input = str(input('Digite uma expressão qualquer que use parênteses: '))
-expression.append(input)
 
-for i, v in enumerate(expression):
-    if v == '(':
-        keys += 1
-    elif v == ')':
-        closekeys += 1    
-
-if '(' not in expression and ')' not in expression:
-    print('Your expression is not avaliable')
-elif closekeys != keys:
-    print('Sua expressão não é válida, algum parentese não foi fechado corretamente')
+for parens in input:
+    if parens == '(':
+        expression.append('(')
+    elif parens == ')':
+        if len(expression) > 0:
+            expression.pop()
+        else:
+            expression.append(')')
+            break 
+if len(expression) == 0:
+    print('Correto')
 else:
-    print("Great job! The expression of yours is avaliable")    
+    print('Incorreto')               
+
+
+    
