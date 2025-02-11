93827d46fd7d0ac27dc0d1193fa522ed239acbc8
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2025-02-11 12:33:28 -0300

    error index out of range

--- Pacote Download/Listas/Listas Compostas/EX 084.py
+++ Pacote Download/Listas/Listas Compostas/EX 084.py
@@ -1,6 +1,7 @@
 main_list = list()
 copy_list = list()
 people = 0
+haviest = lightest = 0
 while True:
     main_list.append(str(input('Digite um nome: ')))
     main_list.append(int(input('Digite o peso: ')))
@@ -8,8 +9,16 @@ while True:
     main_list.clear()
 
     people += 1 # Conta o número de cadastrados
-    for i in range(len(copy_list)): # Contagem de peso 
-        if copy_list[]
+    for i in range(len(copy_list) ): # Contagem de peso 
+        '''Bloco com erro "Index out of range"'''
+        if i == 0:
+            haviest = copy_list[i][i+1]
+            lightest = copy_list[i][i+1]
+        else:
+            if copy_list[i][i+1] > haviest:
+                haviest = copy_list[i][i+1]
+            if copy_list[i][i+1] < lightest:
+                lightest = copy_list[i][i+1]
 
     op = str(input("Deseja continuar? [S/N]: "))
     if op not in 'Ss':
@@ -17,3 +26,5 @@ while True:
 
 print(copy_list)    
 print(f'O Total de pessoas cadastradas: {people}')
+print(f'O Mais pesado foi {haviest}')
+print(f'O Mais leve foi {lightest}')
