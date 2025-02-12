df64c54fe03e77f8a4dc25c58db822b3e158c131
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2025-02-12 17:15:22 -0300

    Libera o baguio ae

--- Pacote Download/Listas/Listas Compostas/EX 084.py
+++ Pacote Download/Listas/Listas Compostas/EX 084.py
@@ -14,18 +14,26 @@ while True:
     if op not in 'Ss':
         break
     
-for i in range(len(copy_list)): # Contagem de peso 
-        '''Bloco com erro "Index out of range"'''
-        if i == 0:
-            haviest = copy_list[i][i+1]
-            lightest = copy_list[i][i+1]
+for i in range(len(copy_list)): # Contagem de peso
+        if j == 0:
+            haviest = copy_list[i][j]
+            lightest = copy_list[i][j]
         else:
-            if copy_list[i][i+1] > haviest:
-                haviest = copy_list[i][i+1]
-            if copy_list[i][i+1] < lightest:
-                lightest = copy_list[i][i+1]    
+            if copy_list[i][j] > haviest:
+                haviest = copy_list[i][j]
+            if copy_list[i][j] < lightest:
+                lightest = copy_list[i][j]   
+        i += 1
 
 print(copy_list)    
 print(f'O Total de pessoas cadastradas: {people}')
-print(f'O Mais pesado foi {haviest}')
-print(f'O Mais leve foi {lightest}')
+
+print(f'O Mais pesado foi {haviest}',end= '')
+for i, v in enumerate(copy_list):
+     if v == haviest:
+        print(v,end= ' e ')
+
+print(f'O Mais leve foi {lightest}',end= '')
+for ii, vv in enumerate(copy_list):
+    if vv == lightest:
+        print(vv,end=' e ')    
