6f00f9bddb93224101d6ebc54d0dddb307eecb90
PedroRSouza0 <pedrorsouzaa032@gmail.com>
2025-02-17 09:17:25 -0300

    update

--- Pacote Download/Pacote Download/Listas Compostas/EX 084.py
+++ Pacote Download/Pacote Download/Listas Compostas/EX 084.py
@@ -1,39 +1,34 @@
+temp = []
 main_list = list()
-copy_list = list()
-people = 0
-haviest = lightest = 0
+maior = menor = 0
+
 while True:
-    main_list.append(str(input('Digite um nome: ')))
-    main_list.append(int(input('Digite o peso: ')))
-    copy_list.append(main_list[:])
-    main_list.clear()
+  temp.append(str(input("Nome: ")))
+  temp.append(float(input('Peso: ')))
+  
+  if len(main_list) == 0: 
+    maior = menor = temp[1]
+  else:
+    if temp[1] > maior:
+      maior = temp[1]
+    if temp[1] < menor:
+      menor = temp[1]
+  main_list.append(temp[:])
+  temp.clear()
 
-    people += 1 # Conta o número de cadastrados
+  op = str(input('Deseja continuar? [S/N]: '))
+  if op not in 'Ss':
+    break
 
-    op = str(input("Deseja continuar? [S/N]: "))
-    if op not in 'Ss':
-        break
-    
-for i in range(len(copy_list)): # Contagem de peso
-        if j == 0:
-            haviest = copy_list[i][j]
-            lightest = copy_list[i][j]
-        else:
-            if copy_list[i][j] > haviest:
-                haviest = copy_list[i][j]
-            if copy_list[i][j] < lightest:
-                lightest = copy_list[i][j]   
-        i += 1
+print()
+print(f'O total de pessoas cadastradas foi de {len(main_list)}')
+print(f'O maior peso foi de {maior}Kg. Peso de',end= '')
 
-print(copy_list)    
-print(f'O Total de pessoas cadastradas: {people}')
+for peso in main_list:
+  if peso[1] == maior:
+    print(f'[{peso[0]}]',end= '')
 
-print(f'O Mais pesado foi {haviest}',end= '')
-for i, v in enumerate(copy_list):
-     if v == haviest:
-        print(v,end= ' e ')
-
-print(f'O Mais leve foi {lightest}',end= '')
-for ii, vv in enumerate(copy_list):
-    if vv == lightest:
-        print(vv,end=' e ')    
+print(f'O menor peso foi de {menor}Kg. Peso de',end= '')
+for peso in main_list:
+  if peso[1] == menor:
+    print(f'[{peso[0]}]',end= '')
