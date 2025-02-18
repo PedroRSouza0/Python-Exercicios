617d89f4e137a509893d1733176d72e73c278542
Pedro Henrique <pedrorsouzaa032@gmail.com>
2025-02-18 16:49:34 -0300

    exercicio maneiro

---
+++ Pacote Download/Pacote Download/Listas Compostas/EX 087.py
@@ -0,0 +1,23 @@
+main_matrix = [[],[],[]]
+somapar = somatres = maior = 0
+#Leitura dos dados
+for i in range(0,3):
+    for j in range(0,3):
+        num = int(input(f'Digite um número para a posição [{i}][{j}]: '))
+        if num % 2 == 0: # Verifica se o número é par e adiciona na soma
+            somapar += num
+        if j == 2: # Soma os números da terceira coluna
+            somatres += num
+        main_matrix[i].append(num)
+
+print('+='*50)
+
+# Saída dos dados
+for i in range(len(main_matrix)): # Estudar o que o -1 tava causando
+    for j in range(len(main_matrix)):
+        print(f'[{main_matrix[i][j]:^5}]',end= ' ')
+    print()
+
+print(f'A soma de todos os números pares da matriz resultou em: {somapar}')
+print(f'A soma de todos os números da terceira coluna foi: {somatres}')
+print(f'O maior valor da segunda linha foi: {maior}')
