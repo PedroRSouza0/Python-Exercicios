e29bfb2d1ca288ef8b28f680ab253144af974aa7
Pedro Souza <pedrorsouzaa032@gmail.com>
2025-04-05 15:23:54 -0300

    Ordenando

--- Pacote Download/Pacote Download/Dictionaries/EX 90 (media).py
+++ Pacote Download/Pacote Download/Dictionaries/EX 90 (media).py
@@ -3,10 +3,11 @@ dic = dict()
 dic['Name'] = str(input("Write your name: "))
 dic['Average'] = float(input('Put your average: '))
 
-if dic['Average'] >= 7:
+if dic['Average'] >= 7: # See if the student is aproved ou not
     dic['Situation'] = 'Aproved'
 else:
     dic['Situation'] = 'Reproved'
 
+# Print the structure of the dictionary , showing all the items
 for k, v in dic.items():
     print(f'{k} = {v}')
