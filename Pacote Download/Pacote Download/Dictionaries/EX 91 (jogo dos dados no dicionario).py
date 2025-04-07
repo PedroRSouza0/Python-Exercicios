e6e7d1770023a0f594e20faa3d5a5f3fff4b7805
Pedro Souza <pedrorsouzaa032@gmail.com>
2025-04-06 22:11:08 -0300

    Improving

---
+++ Pacote Download/Pacote Download/Dictionaries/EX 91 (jogo dos dados no dicionario).py
@@ -0,0 +1,22 @@
+from random import randint
+from time import sleep
+
+# I have declared a dictionary
+dic = dict() 
+# Input of the values using and creating into the for the keys as well
+for i in range(1,4+1):
+    dic[i] = randint(1,6)
+# Output    
+for k, v in dic.items():
+    print("{} got {}".format(k,v))
+    sleep(1)
+
+print()
+print("Player's Ranking:")
+print()
+#Ordering the dictionary
+for v in dic.values():
+    for value in dic.values(): #Error, but this is a selection sort
+        if v > value:
+            value, v = v, value
+print(dic)            
