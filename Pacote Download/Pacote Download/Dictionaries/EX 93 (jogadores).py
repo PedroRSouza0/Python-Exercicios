322a6a40608ba75486733a2f8fd043ac008e5f7a
Pedro Souza <pedrorsouzaa032@gmail.com>
2025-04-09 11:26:48 -0300

    EX93

---
+++ Pacote Download/Pacote Download/Dictionaries/EX 93 (jogadores).py
@@ -0,0 +1,22 @@
+gols = list()
+data = dict()
+total = 0
+
+data['Nome'] = str(input('Digite o nome: '))
+data['Partidas'] = int(input(f'Quantas partidas o {data["Nome"]} jogou?: '))
+# Número de Partidas
+for i in range(data['Partidas']):
+    gols.append(int(input(f"Quantos gols na partida {i}: "))) # Lê os gols em cada partida
+    total += gols[i]
+
+data['Golls'] = gols #Insere a lista no dicionário
+data['Total'] = total
+print(data)
+print()
+for k, v in data.items():
+    print(f'{k} = {v}')
+print()    
+print(f'O jogador jogou {data['Partidas']} partidas') # Arrumar indice
+for i in range(len(gols)):
+    print(f'Na partida {i} marcou: {gols[i]}')
+print(f'Totalizando {total}')    
