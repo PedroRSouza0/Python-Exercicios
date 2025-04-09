341fe0a73de0b9bd7c5232a16651e242bcf329a1
Pedro Souza <pedrorsouzaa032@gmail.com>
2025-04-09 11:02:21 -0300

    Ajustes

--- Pacote Download/Pacote Download/Dictionaries/EX 92 (aposentadoria).py
+++ Pacote Download/Pacote Download/Dictionaries/EX 92 (aposentadoria).py
@@ -6,14 +6,10 @@ age = 2025 - birth_year
 dic['Age'] = age
 dic['CTPS'] = int(input('Write here your CTPS. [If you do not have one click [0]]: '))
 
-if dic['CTPS'] == 0:
-   print()
-   for k, v in dic.items():        
-        print(f'{k} = {v}')
-else:
-    year_that_you_started = int(input('Ano de contratação: '))
+if dic['CTPS'] != 0:
+    dic['year_that_you_started'] = int(input('Ano de contratação: '))
     dic['Salary'] = float(input('Salary: '))
-    dic['aposentadoria'] = 2025 - year_that_you_started 
+    dic['aposentadoria'] = dic['Age'] + ((dic['year_that_you_started'] + 35) - 2025) #Calcula a idade da aposentadoria
     print()
-    for k, v in dic.items():
-        print(f'{k} = {v}')
+for k, v in dic.items():
+    print(f'{k} = {v}')
