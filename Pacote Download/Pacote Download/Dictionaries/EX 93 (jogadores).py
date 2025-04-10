cb0c57508b33214cd5a415d8a2e390963d214429
Pedro Souza <pedrorsouzaa032@gmail.com>
2025-04-10 09:50:59 -0300

    Corrigindo once again

--- Pacote Download/Pacote Download/Dictionaries/EX 93 (jogadores).py
+++ Pacote Download/Pacote Download/Dictionaries/EX 93 (jogadores).py
@@ -7,9 +7,9 @@ data['Partidas'] = int(input(f'Quantas partidas o {data["Nome"]} jogou?: '))
 # Número de Partidas
 for i in range(data['Partidas']):
     gols.append(int(input(f"Quantos gols na partida {i}: "))) # Lê os gols em cada partida
-    total += gols[i]
+    total += gols[i] # Soma o total
 
-data['Golls'] = gols #Insere a lista no dicionário
+data['Golls'] = gols[:] #Insere a lista no dicionário
 data['Total'] = total
 print(data)
 print()
@@ -18,6 +18,6 @@ for k, v in data.items():
     print(f'{k} = {v}')
 print()    
 print(f'O jogador {data['Nome']} jogou {data['Partidas']} partidas') # Arrumar indice
-for i in range(len(gols)):
+for i in range(len(gols)): # Podria ser enumerate porque estamos utilizando uma lista
     print(f'Na partida {i} marcou: {gols[i]}')
 print(f'Totalizando {total}')    
