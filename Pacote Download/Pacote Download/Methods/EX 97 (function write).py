7aabf12c94545b59a7de88e8aa883f5a691ae87e
Pedro Souza <pedrorsouzaa032@gmail.com>
2025-04-22 15:18:50 -0300

    Formatação

--- Pacote Download/Pacote Download/Methods/EX 97 (function write).py
+++ Pacote Download/Pacote Download/Methods/EX 97 (function write).py
@@ -1,7 +1,8 @@
 def Write(string):
-    print('~'*len(string))
-    print(string)
-    print('~'*len(string))
+    tam = len(string) + 4
+    print('~'* tam) 
+    print(f'  {string}')
+    print('~'* tam) 
 
 
 Write('meu palmeiras meu palmeiras vocÊ é meu bem querer')
