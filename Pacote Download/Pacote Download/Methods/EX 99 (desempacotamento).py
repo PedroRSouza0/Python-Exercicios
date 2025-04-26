405d9479cf5d7021f0ef7c4056535c8e02e64cec
Pedro Souza <pedrorsouzaa032@gmail.com>
2025-04-26 20:30:33 -0300

    Desempacotamento de parametros

---
+++ Pacote Download/Pacote Download/Methods/EX 99 (desempacotamento).py
@@ -0,0 +1,19 @@
+from time import sleep
+
+def maior(*num):
+    print('-='*20)
+    Maior = num[0] # Primeiro numero
+    total = len(num)
+    print(f'The chosen numbers were {total}: ',end= '')
+    for i in num:
+        if i > Maior:
+            Maior = i
+        print(i, end= ' ', flush=True)
+        sleep(0.5)
+    print(f'\nThe bigger one was: {Maior}')    
+    print('-='*20)
+
+
+maior(6)
+maior(1,5,2,400,123)
+maior(5,5,1)
